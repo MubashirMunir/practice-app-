9a9ed672214460a019de05ccfd6a9966ff5a8b9e
MubashirMunir <mubasharmunir000@gmail.com>
2024-01-12 23:09:02 +0500

    initial

--- lib/page/contactUs.dart
+++ lib/page/contactUs.dart
@@ -5,7 +5,7 @@ class contact extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
           children: [IconButton(onPressed: null, icon: Icon(Icons.facebook))]),
     );
